16d845b43a0564664c3e114cecc456d53c4b0a9c
AjinkyaAmbulgekar2 <41302977+AjinkyaAmbulgekar2@users.noreply.github.com>
2021-04-30 17:36:11 +0530

    Create (optional) Extension.py

---
+++ Assignment 2/(optional) Extension.py
@@ -0,0 +1,12 @@
+def main():
+    num = float(input("Enter: "))
+    num2 = 0
+    while num >= num2:
+        num2 = num
+        num = float(input("Enter: "))
+
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == '__main__':
+    main()
