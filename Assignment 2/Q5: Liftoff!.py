d36ae5244dfdeee89852d87a8a66b0c82bfecf84
AjinkyaAmbulgekar2 <41302977+AjinkyaAmbulgekar2@users.noreply.github.com>
2021-04-30 17:38:56 +0530

    Create Q5: Liftoff!.py

---
+++ Assignment 2/Q5: Liftoff!.py
@@ -0,0 +1,14 @@
+Q5: Liftoff!
+
+
+def main():
+    countdown = 10
+
+    for i in range(10):
+        print(countdown)
+        countdown -= 1
+    print("Liftoff!")
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == "__main__":
+    main()
