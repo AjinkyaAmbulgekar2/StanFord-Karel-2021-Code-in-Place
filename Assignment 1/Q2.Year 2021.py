9f48c773a6bed03f52ce28428688c15484c0c2b1
AjinkyaAmbulgekar2 <41302977+AjinkyaAmbulgekar2@users.noreply.github.com>
2021-04-30 17:29:59 +0530

    Create Q2.Year 2021.py

---
+++ Assignment 1/Q2.Year 2021.py
@@ -0,0 +1,11 @@
+Q2: Year 2021
+
+from karel.stanfordkarel import *
+def main():
+    for i in range(20):
+        put_beeper()
+    move()
+    put_beeper()
+    for i in range(20):
+        put_beeper()
+    move()
