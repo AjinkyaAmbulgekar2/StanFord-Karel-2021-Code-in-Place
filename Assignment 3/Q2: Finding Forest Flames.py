33c34910a8daaeef3cf4ed2d34895433f0d23e94
Ajinkya Ambulgekar <41302977+AjinkyaAmbulgekar2@users.noreply.github.com>
2021-05-13 11:54:47 +0530

    Create Q2: Finding Forest Flames.py

---
+++ Assignment 3/Q2: Finding Forest Flames.py
@@ -0,0 +1,50 @@
+Q2: Finding Forest Flames
+  
+  
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/greenland-fire.png'
+
+def find_flames(filename):
+    """
+    This function should highlight the "sufficiently red" pixels
+    in the image and grayscale all other pixels in the image
+    in order to highlight areas of wildfires.
+    """
+    image = SimpleImage(filename)
+    for pixel in image:
+        pix_ave = (pixel.red + pixel.green + pixel.blue) // 3
+        if pixel.red > pix_ave:
+            pixel.red = 255
+            pixel.green = 0
+            pixel.blue = 0
+        else:
+            pixel.red = pix_ave
+            pixel.green = pix_ave
+            pixel.blue = pix_ave
+    return image
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original fire
+    original_fire = SimpleImage(filename)
+    original_fire.show()
+
+    # Show the highlighted fire
+    highlighted_fire = find_flames(filename)
+    highlighted_fire.show()
+
+    
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
